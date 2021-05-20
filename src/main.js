770645db161c45bdbf794efaf5d0f7a57cf789ef
laura <997027905@qq.com>
2021-05-20 10:17:45 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import axios from 'axios'
+import './assets/CSS/global.css'   //导入全局样式
+import './assets/fonts/iconfont.css' //导入字体图标
 
+Vue.prototype.$http = axios
+//配置请求的根路径,后期需要将axios进行封装管理
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
